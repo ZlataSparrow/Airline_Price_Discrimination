20e99ab7827ac42f065554567a9779f7965faf6c
Zlatagaga <zlatasparrow@gmail.com>
2025-10-08 12:15:22 -0700

    Update .gitignore to exclude CSV files, modify output path in fetch_data_ready.py, and reset execution counts in data preparation notebooks for consistency.

--- fetch_data_ready.py
+++ fetch_data_ready.py
@@ -1,11 +1,15 @@
 import gdown
 import os
 
+# Make sure the "data" folder exists
 os.makedirs("data", exist_ok=True)
 
+# File ID from your Google Drive link
 file_id = "1Dp6wkFBORO3D4W2sy5fQsTktVDChlRuH"
 url = f"https://drive.google.com/uc?id={file_id}"
 
-output_path = "data/Airlines_ticket_data_ready.pkl"
+# Output path where the file will be saved
+output_path = "data/Airlines_tickets_data_ready.pkl"
 
+# Download the file
 gdown.download(url, output_path, quiet=False)
