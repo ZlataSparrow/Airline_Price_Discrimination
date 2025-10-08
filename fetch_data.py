c18b7eec502af40a9298c176367ceb2e5a2a5624
Zlatagaga <zlatasparrow@gmail.com>
2025-10-07 19:47:54 -0700

    Update data preparation notebook to handle missing data file and adjust execution counts; remove fetch_data script as it is no longer needed.

--- fetch_data.py
+++ fetch_data.py
@@ -9,4 +9,4 @@ file_id = "1JZBrVIJ3dtp7gw4dnpz_SNyEY3p2v4k0"
 url = f"https://drive.google.com/uc?id={file_id}"
 
 # Download the file
-gdown.download(url, "data/Airlines_ticket_data_ready.pkl", quiet=False)
+gdown.download(url, "data/Airline_tickets_data.pkl", quiet=False)
